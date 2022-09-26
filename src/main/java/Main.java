fb56a8da7a97c8154d79ea91d3871d448e91115d
FalkFlake <Falk.flake@gmail.com>
2022-09-26 13:18:36 +0200

    add Class Second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
+
+    private static Second sec = new Second();
+
     public static void main(String[] args) {
+
         System.out.println("POG");
         System.out.println("vers 1");
         System.out.println("testbranch2");
         System.out.println("Und der andere test auf branch 2");
+
+       sec.Sec();
+
     }
 }
